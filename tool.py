41c22192b3734e6062ee1c94f133b6dd8b5d047d
Shawn Wilkinson <me@super3.org>
2014-07-17 14:14:26 -0400

    Added Back Tool

---
+++ tool.py
@@ -0,0 +1,25 @@
+import argparse
+from Chunk import Chunk
+from Streamer import Streamer
+
+# Parser Object
+parser = argparse.ArgumentParser()
+
+# Add Arguments
+parser.add_argument('action')
+parser.add_argument('-s', '--server', default="http://node1.storj.io")
+parser.add_argument('-f', '--filepath')
+parser.add_argument('-u', '--uri')
+parser.add_argument('-d', '--destination')
+
+# Get Args
+args = parser.parse_args()
+
+# Do Commands
+stream = Streamer("http://node1.storj.io")
+if args.action == "upload":
+	chunk_list, shredder_data = stream.upload(args.filepath)
+elif args.action == "download":
+	chunk = [Chunk().load_uri(args.uri)]
+	if args.destination == "": result = stream.download(chunk)
+	else: result = stream.download(chunk, None, args.destination)
