72027f29beeae6fab754739b05b53fb739ed5ade
Paul Durivage <pauldurivage@gmail.com>
2014-08-06 20:36:02 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='upstream',
+    version='0.1.0',
+    url='https://github.com/Storj/upstream',
+    license='MIT',
+    author='Storj Labs',
+    author_email='info@storj.io',
+    description='Command line tool for uploading and downloading files from Metadisk',
+    packages=['upstream'],
+
+)
