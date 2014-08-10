8f9c9407ac51c6047036154769ed149ce26cc849
Paul Durivage <pauldurivage@gmail.com>
2014-08-10 12:12:12 -0500

    Add custom exceptions

---
+++ upstream/exc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2014 Paul Durivage <paul.durivage@gmail.com>
+# All Rights Reserved
+
+
+class FileError(Exception):
+    pass
+
+
+class ResponseError(Exception):
+    pass
+
+
+class ConnectError(Exception):
+    pass
