c47619ddd0e209f813ecae94b225f150bd41a1c4
Paul Durivage <pauldurivage@gmail.com>
2014-08-10 12:11:13 -0500

    Rewrite upload method with new exceptions, adding form_encoded and chunked encoded methods, docstrings, etc

--- upstream/streamer.py
+++ upstream/streamer.py
@@ -51,31 +51,88 @@ class Streamer(object):
         """
         # Open the file and upload it via POST
         url = self.server + "/api/upload"  # web-core API
-        # This is a Transfer-Encoding: chunked filechunk generator
-        # that's disabled for fun, also cuz broken APO
-        # r = requests.post(url, data=self.filestream(path))
-        m = MultipartEncoder(
-            {
-                'file': ('testfile', open('one-meg.testfile', 'rb'))
-            }
-        )
-        r = requests.post(url, data=m, headers={'Content-Type': m.content_type})
+        r = self._upload_form_encoded(url, filepath)
 
         # Make sure that the API call is actually there
         if r.status_code == 404:
-            raise LookupError("API call not found.")
+            raise ResponseError("API call not found.")
         elif r.status_code == 402:
-            raise LookupError("Payment required.")
+            raise ResponseError("Payment required.")
         elif r.status_code == 500:
-            raise LookupError("Server error.")
+            raise ResponseError("Server error.")
         elif r.status_code == 201:
             # Everthing checked out, return result
             # based on the format selected
-            return Chunk().load_json(r.text)
+            chunk = Chunk()
+            return chunk.load_json(r.text)
         else:
-            raise LookupError("Received status code %s %s "
-                              % (r.status_code, r.reason))
+            raise ResponseError("Received status code %s %s"
+                                % (r.status_code, r.reason))
 
+    def download(self, chunk_list, shredder_data=None, destination=""):
+        """Download a chunk via GET from the specified node.
+
+        :param chunk_list:
+        :param shredder_data:
+        :param destination:
+        """
+        if len(chunk_list) <= 0:
+            pass
+        elif len(chunk_list) == 1:
+            self._download_chunk(chunk_list[0], destination)
+        else:
+            for chunk in chunk_list:
+                self._download_chunk(chunk, "download/" + chunk.filename)
+            shredder_data.merge_chunks()
+
+    def check_path(self, filepath):
+        """ Expands and validates a given path to a file and returns it
+
+        :param filepath: Path to file as string
+        :return: Expanded validated path as string
+        :raise FileError: If path is not a file or does not exist
+        """
+        expandedpath = os.path.expanduser(filepath)
+        try:
+            assert os.path.isfile(expandedpath)
+        except AssertionError:
+            raise FileError("%s not a file or not found" % filepath)
+        return expandedpath
+
+    def _upload_form_encoded(self, url, filepath):
+        """ Streams file from disk and uploads it.
+
+        :param url: API endpoint as URL to upload to
+        :param filepath: Path to file as string
+        :return: requests.Response
+        """
+        validpath = self.check_path(filepath)
+        m = MultipartEncoder(
+            {
+                'file': ('testfile', open(validpath, 'rb'))
+            }
+        )
+        headers = {
+            'Content-Type': m.content_type
+        }
+        return requests.post(url, data=m, headers=headers)
+
+    def _upload_chunked_encoded(self, url, filepath):
+        """ Uploads a file using chunked transfer encoding.
+        web-core does not currently accept this type of uploads because
+        of issues in upstream projects, primariy flask and werkzeug.  Leaving
+        it here for posterity as it might be useful in the future.
+        This function currently rases a NotImplementedError currently becuase
+        it's purposely "deactivated".
+
+        :param url: API endpoint as URL to upload to
+        :param filepath: Path to file as string
+        :return: requests.Response
+        :raise NotImplementedError: Raises this error on any call.
+        """
+        # validpath = self._check_path(filepath)
+        # return requests.post(url, data=self._filestream(validpath))
+        raise NotImplementedError
 
     def _download_chunk(self, chunk, destination=""):
         """ Download a chunk via GET from the specified node.
